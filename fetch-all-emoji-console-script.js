a82b0af7b22a37ec2d43ff4820e70da22ad669ae
Aarni Koskela <akx@iki.fi>
2025-01-08 13:44:19 +0200

    Add progress indication to console script

--- fetch-all-emoji-console-script.js
+++ fetch-all-emoji-console-script.js
@@ -5,12 +5,14 @@ async function fetchAllEmoji() {
 		fd.append('page', page);
 		fd.append('count', 200);
 		fd.append('token', boot_data.api_token);
+		console.log(`Fetching page ${page}`);
 		const resp = await fetch('/api/emoji.adminList', {credentials: "include", method: "POST", body: fd});
 		const json = await resp.json();
 		const pEmoji = json.emoji || [];
 		emoji = emoji.concat(pEmoji);
 		if(!pEmoji.length) break;
 	}
+	console.log(`Fetched ${emoji.length} emoji!`);
 	return emoji;
 }
 copy(await fetchAllEmoji());
